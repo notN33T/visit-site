671f93f3fd61b9f86c3c62023b28c5c201677f4b
notN33T <52465709+notN33T@users.noreply.github.com>
2022-01-01 14:01:41 +0300

    Done HomePage, starting AboutPage

--- client/src/pages/about/AboutPage.tsx
+++ client/src/pages/about/AboutPage.tsx
@@ -1,5 +1,6 @@
 import { useState }        from 'react'
 import Loading             from '../../pages/common/Loading/Loading'
+import './css/about.css'
 
 export default function AboutPage() {
     let [isLoaded, setIsLoaded] = useState(false)
@@ -8,8 +9,25 @@ export default function AboutPage() {
     if(!isLoaded) return <Loading/> 
 
     return (
-        <div>
-            About page
+        <div className='about-c'>
+            <div className='about-info-c'>
+                <div className='about-title-c'>
+                    <p className='html-txt'>{'<h1>'}</p>
+                    <p className='about-title'>About me</p>
+                    <p className='html-txt'>{'</h1>'}</p>
+                </div>
+                <div className="about-text-c">
+                    <p className='html-txt'>{'</p>'}</p>
+                    <p className='about-text'>I am begginer web developer from Belarus.
+                    At this moment i leave in the city of Minsk. Also, im third year
+                    student of the university BNTU.</p>
+                    <p className='html-txt'>{'</p>'}</p>
+                </div>
+            </div>
+
+            <div className='about-tech-c'>
+                CSS SASS NODE.JS REACT REDUX EXPRESS HTML MONGODB MYSQL
+            </div>
         </div>
     )
 }
