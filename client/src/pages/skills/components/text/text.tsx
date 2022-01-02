6b4d814db2cc45ad0f832cbae3146283ee14e959
notN33T <52465709+notN33T@users.noreply.github.com>
2022-01-02 18:16:51 +0300

    begining SkillsPage

---
+++ client/src/pages/skills/components/text/text.tsx
@@ -0,0 +1,14 @@
+import './css/text.css'
+
+export default function Text() {
+    return (
+        <div className='text-c'>
+            <div className="text-title">
+        
+            </div>
+            <div className="text-txt">
+
+            </div>
+        </div>
+    )
+}
