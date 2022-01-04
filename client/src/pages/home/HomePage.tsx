27df8ed59c4023afea03e639ac59e2ce0469f716
notN33T <52465709+notN33T@users.noreply.github.com>
2022-01-05 00:19:42 +0300

    Some changes on stats.css

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect }  from 'react'
+import { useState }        from 'react'
 import Loading             from '../../pages/common/Loading/Loading'
 import Greeting            from './Components/Greeting/Greeting'
 import './css/home.css'
