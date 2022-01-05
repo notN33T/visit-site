37a5082373d71a065da57cc2a9e1067fb0101cb1
notN33T <52465709+notN33T@users.noreply.github.com>
2022-01-05 21:24:37 +0300

    SomeFixes, +buid v1 +.gitignore +server.js

--- client/src/pages/contact/ContactPage.tsx
+++ client/src/pages/contact/ContactPage.tsx
@@ -1,13 +1,35 @@
 import { useState }        from 'react'
 import Loading             from '../../pages/common/Loading/Loading'
+import './css/contact.css'
 export default function ContactPage() {
     let [isLoaded, setIsLoaded] = useState(false)
     setTimeout( () => {setIsLoaded(true)}, 500)
     if(!isLoaded) return <Loading/> 
     
     return (
-        <div>
-            Contact page
+        <div className='contact-c'>
+            <div className='contact-txt-c'>
+                <p className='contact-title-txt'>
+                    Contact me
+                </p>
+                <p className='contact-discr-txt'>
+                    To cantact me, you can use next ways:   
+                </p>
+            </div>
+            <div className='contact-links-c'>
+                <a className='cont-icons'>
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 352c-16.53 0-33.06-5.422-47.16-16.41L0 173.2V400C0 426.5 21.49 448 48 448h416c26.51 0 48-21.49 48-48V173.2l-208.8 162.5C289.1 346.6 272.5 352 256 352zM16.29 145.3l212.2 165.1c16.19 12.6 38.87 12.6 55.06 0l212.2-165.1C505.1 137.3 512 125 512 112C512 85.49 490.5 64 464 64h-416C21.49 64 0 85.49 0 112C0 125 6.01 137.3 16.29 145.3z"></path></svg>
+                    <a className='contact-tg-ic'>ztrubish@mail.ru</a>
+                </a>     
+                <a className='cont-icons' href='https://t.me/Evgenyptn'>
+                    <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="telegram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256S111 504 248 504 496 392.1 496 256 384.1 8 248 8zM362.1 176.7c-3.732 39.22-19.88 134.4-28.1 178.3-3.476 18.58-10.32 24.82-16.95 25.42-14.4 1.326-25.34-9.517-39.29-18.66-21.83-14.31-34.16-23.22-55.35-37.18-24.49-16.14-8.612-25 5.342-39.5 3.652-3.793 67.11-61.51 68.33-66.75 .153-.655 .3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283 .746-104.6 69.14-14.85 10.19-26.89 9.934c-8.855-.191-25.89-5.006-38.55-9.123-15.53-5.048-27.88-7.717-26.8-16.29q.84-6.7 18.45-13.7 108.4-47.25 144.6-62.3c68.87-28.65 83.18-33.62 92.51-33.79 2.052-.034 6.639 .474 9.61 2.885a10.45 10.45 0 0 1 3.53 6.716A43.76 43.76 0 0 1 362.1 176.7z"></path></svg>
+                    <a className='contact-tg-ic'>telegram</a>
+                </a>   
+                <a className='cont-icons' href='https://www.linkedin.com/in/evgeny-trubish-b8419b229/'>
+                    <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path></svg>
+                    <a className='contact-tg-ic'>linkedin</a>
+                </a>     
+            </div>
         </div>
     )
 }
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const PORT = 5000
+
+const app = express()
+
+app.set('views', './public')
+app.use(express.static( __dirname + '/public'))
+
+app.get('/*', (req, res) => { 
+    res.sendFile('./public/index.html', { root: __dirname })
+})
+
+app.listen(PORT, ()=> {
+    console.log(__dirname)
+})
